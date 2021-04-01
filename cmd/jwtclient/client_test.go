12b9423196292ed34001dc19d2f144a135ad07e6
Rennbon <343688972@qq.com>
2021-04-01 13:44:40 +0800

    no message

--- cmd/jwtclient/client_test.go
+++ cmd/jwtclient/client_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 )
 
-var MockCli *Client
+var MockCli *JWTClient
 
 func TestMain(m *testing.M) {
 	defer os.Exit(0)
-	MockCli = NewClient("http://localhost:8989")
+	MockCli = NewJWTClient("http://localhost:8989")
 	m.Run()
 }
 
@@ -17,7 +17,7 @@ func TestClient_Verify(t *testing.T) {
 	if os.Getenv("CI") == "test" {
 		t.Skip()
 	}
-	res, err := MockCli.Verify("miner-1", "192.168.22.22", "192.168.22.21", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.q3xz5oucOoT3xwMTct8pWMBrvhi_gizOz6QBgK-nOwc")
+	res, err := MockCli.Verify("miner-1", "mockSvc", "192.168.22.22", "192.168.22.21", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.q3xz5oucOoT3xwMTct8pWMBrvhi_gizOz6QBgK-nOwc")
 	if err != nil {
 		t.Fatal(err)
 	}

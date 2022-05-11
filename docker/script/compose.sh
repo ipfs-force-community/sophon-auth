ea77527ea1f3eac65246e1e86501f9e2f2769dc9
tanlang <lin.zexiao@qq.com>
2022-05-11 12:24:46 +0800

    add: more readable log

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -3,11 +3,14 @@
 echo $@
 /app/venus-auth run &
 
-sleep 3
+sleep 1
 
 echo "regist admin"
 /app/venus-auth user add --name="admin"
 token=`/app/venus-auth token gen --perm admin admin`
+/app/venus-auth  user active admin
+
+echo "token:"
 echo ${token#*: }
 echo "${token#*: }" > /env/token
 

ca344614c794582692fbb58511e7c290ca065002
tanlang <lin.zexiao@qq.com>
2022-12-15 03:36:47 +0000

    feat: add util to check perm for user isolation

---
+++ jwtclient/utils.go
@@ -0,0 +1,96 @@
+package jwtclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-jsonrpc/auth"
+	"github.com/filecoin-project/venus-auth/core"
+)
+
+var (
+	// ErrorPermissionDeny is the error message returned when a user does not have permission to perform an action
+	ErrorPermissionDeny = fmt.Errorf("permission deny")
+
+	// ErrorUserNotFound is the error message returned when a user is not found in context
+	ErrorUserNotFound = fmt.Errorf("user not found")
+)
+
+// checkPermissionByUser check weather the user has admin permission or is match the username passed in
+func CheckPermissionByName(ctx context.Context, name string) error {
+	if auth.HasPerm(ctx, []auth.Permission{}, core.PermAdmin) {
+		return nil
+	}
+	user, exist := CtxGetName(ctx)
+	if !exist || user != name {
+		return ErrorPermissionDeny
+	}
+	return nil
+}
+
+func CheckPermissionBySigner(ctx context.Context, client IAuthClient, addrs ...address.Address) error {
+	if auth.HasPerm(ctx, []auth.Permission{}, core.PermAdmin) {
+		return nil
+	}
+	user, exist := CtxGetName(ctx)
+	if !exist {
+		return ErrorUserNotFound
+	}
+
+	for _, wAddr := range addrs {
+		ok, err := client.SignerExistInUser(ctx, user, wAddr)
+		if err != nil {
+			return fmt.Errorf("check signer exist in user fail %s failed when check permission: %s", wAddr.String(), err)
+		}
+		if !ok {
+			return ErrorPermissionDeny
+		}
+	}
+	return nil
+}
+
+func CheckPermissionByMiner(ctx context.Context, client IAuthClient, addrs ...address.Address) error {
+	if auth.HasPerm(ctx, []auth.Permission{}, core.PermAdmin) {
+		return nil
+	}
+	user, exist := CtxGetName(ctx)
+	if !exist {
+		return ErrorUserNotFound
+	}
+	for _, mAddr := range addrs {
+		ok, err := client.MinerExistInUser(ctx, user, mAddr)
+		if err != nil {
+			return fmt.Errorf("check miner exist in user fail %s failed when check permission: %s", mAddr.String(), err)
+		}
+		if !ok {
+			return ErrorPermissionDeny
+		}
+	}
+	return nil
+}
+
+// isAdmin check if the user is admin and return signers of the user
+func IsAdmin(ctx context.Context, client IAuthClient) (isAdmin bool, signers []address.Address, err error) {
+
+	signers = []address.Address{}
+	err = nil
+
+	if auth.HasPerm(ctx, []auth.Permission{}, core.PermAdmin) {
+		return true, nil, nil
+	}
+	user, exit := CtxGetName(ctx)
+	if !exit && !isAdmin {
+		err = ErrorUserNotFound
+		return false, nil, err
+	}
+	resp, err := client.ListSigners(ctx, user)
+	if err != nil {
+		return false, nil, err
+	}
+	for _, res := range resp {
+		signers = append(signers, res.Signer)
+	}
+
+	return false, signers, nil
+}
